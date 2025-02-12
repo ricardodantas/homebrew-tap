d53616211b36e5fc5a343e2d5769020bf337e01d
Ricardo Dantas <86023+ricardodantas@users.noreply.github.com>
2025-02-12 14:18:27 +0100

    Update FolderDeclutter formula to use tar.gz URL and updated SHA256

--- Formula/folder-declutter.rb
+++ Formula/folder-declutter.rb
@@ -1,8 +1,8 @@
 class FolderDeclutter < Formula
     desc "Folder Declutter is a command-line and library utility written in Rust that helps you free up disk space by automatically deleting files that haven't been modified in a specified number of days."
     homepage "https://github.com/ricardodantas/folder-declutter"
-    url "https://github.com/ricardodantas/folder-declutter/releases/download/v0.4.0/folder-declutter-macos-0.4.0"
-    sha256 "44e4369d5752597a67638b981d3ccc1ae06d3a7d35ed1d00b73a6debd4b13d40"
+    url "https://github.com/ricardodantas/folder-declutter/archive/refs/tags/v0.4.0.tar.gz"
+    sha256 "98073a3002dc6013260599c2fe1c4b8a82a028c9b88566e578ffd2d0479434f2"
 
     depends_on "rust" => :build
 
