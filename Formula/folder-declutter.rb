5290f43db0cad9ad9ed818e6883eb3e13f5e61d8
Ricardo Dantas <86023+ricardodantas@users.noreply.github.com>
2025-02-12 16:01:52 +0100

    folder-declutter 0.4.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/folder-declutter.rb
+++ Formula/folder-declutter.rb
@@ -1,8 +1,8 @@
 class FolderDeclutter < Formula
     desc "Folder Declutter is a command-line and library utility written in Rust that helps you free up disk space by automatically deleting files that haven't been modified in a specified number of days."
     homepage "https://github.com/ricardodantas/folder-declutter"
-    url "https://github.com/ricardodantas/folder-declutter/archive/refs/tags/v0.4.1.tar.gz"
-    sha256 "0b72c299ec4b863d4136044a6509a90e6029a2be361de056ffb9bea6dc07c66b"
+    url "https://github.com/ricardodantas/folder-declutter/archive/refs/tags/v0.4.6.tar.gz"
+    sha256 "ce7523ee82cb6d00bcce1bcce0cae0d9065fbb2f1b037d3142030deeb2347e5c"
 
     depends_on "rust" => :build
 
